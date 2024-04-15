d97437bfce048fa0ea5d2a86f8d76934659c1d1a
Simone Manti <manti998@gmail.com>
2024-04-15 13:44:56 +0200

    Implemented changes to sr_rom_closure to account for real world data.

--- src/sr_rom/data/data.py
+++ src/sr_rom/data/data.py
@@ -22,20 +22,20 @@ def toy_data(k: float, r: int) -> Tuple[npt.NDArray, npt.NDArray]:
 
 
 def generate_toy_data(r):
-    k_array = np.arange(1, 11)
+    k_list = list(range(1, 11))
     A_B_list = []
-    for k in k_array:
+    for k in k_list:
         A_B_k = toy_data(k, r)
         A_B_list.append(A_B_k)
 
-    return k_array, A_B_list
+    return k_list, A_B_list
 
 
 def split_data(k_array, A_B_list):
     k_train_val, k_test, A_B_train_val, A_B_test = ttsplit(
-        k_array, A_B_list, test_size=0.2, random_state=42, shuffle=True)
+        k_array, A_B_list, test_size=0.2, random_state=42, shuffle=False)
     k_train, k_val, A_B_train,  A_B_val = ttsplit(
-        k_train_val, A_B_train_val, test_size=3/8, random_state=42, shuffle=True)
+        k_train_val, A_B_train_val, test_size=3/8, random_state=42, shuffle=False)
 
     train_data = Dataset("k_A_B", k_train, A_B_train)
     val_data = Dataset("k_A_B", k_val, A_B_val)
@@ -54,9 +54,12 @@ def process_data(r: int, bench_name: str) -> Tuple[Dataset, Dataset, Dataset]:
         k = float(directory.replace("Re", ""))
         A = np.loadtxt(directory_path+"/tildeA_N5", delimiter=',', usecols=range(r))
         B = np.loadtxt(directory_path+"/tildeB_N5", delimiter=',', usecols=range(r))
+        # normalize A and B
+        A = (A - np.min(A))/(np.max(A) - np.min(A))
+        B = (B - np.min(B))/(np.max(B) - np.min(B))
         k_list.append(k)
         A_B_list.append({'A': A, 'B': B})
-    return np.array(k_list), A_B_list
+    return k_list, A_B_list
 
 
 if __name__ == "__main__":
--- src/sr_rom/sr_rom_closure.py
+++ src/sr_rom/sr_rom_closure.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sr_rom.data.data import generate_toy_data, split_data
+from sr_rom.data.data import process_data, split_data
 from alpine.data import Dataset
 from alpine.gp import gpsymbreg as gps
 from typing import Callable, Tuple, List
@@ -30,6 +30,8 @@ def eval_MSE_sol(individual: Callable, indlen: int,
     for i, k in enumerate(k_array):
         component_computed = individual(k)
         total_error += 1/len(k_array)*(component_computed - component_list[i])**2
+        # total_error += (component_computed - component_list[i])**2
+        # total_error += np.abs(component_computed - component_list[i])
         component_computed_list.append(component_computed)
 
     if np.isnan(total_error) or total_error > 1e6:
@@ -79,44 +81,79 @@ def fitness(individuals_batch: list[gp.PrimitiveSet], indlen: int, toolbox: Tool
 
 
 def sr_rom(config_file_data, train_data, val_data, test_data):
+    best_ind_str = []
+    ts_scores = np.zeros((5, 5), dtype=np.float64)
+
+    # for i in range(5):
+    #    for j in range(5):
     i = 0
-    j = 2
+    j = 0
+
     train_A_i_j = [A_B['A'][i, j]for A_B in train_data.y]
     val_A_i_j = [A_B['A'][i, j]for A_B in val_data.y]
     test_A_i_j = [A_B['A'][i, j]for A_B in test_data.y]
+    train_val_A_i_j = train_A_i_j + val_A_i_j
     train_data_i_j = Dataset("k_component", train_data.X, train_A_i_j)
     val_data_i_j = Dataset("k_component", val_data.X, val_A_i_j)
     test_data_i_j = Dataset("k_component", test_data.X, test_A_i_j)
+    train_val_data_i_j = Dataset(
+        "k_component", train_data.X + val_data.X, train_val_A_i_j)
 
     pset = gp.PrimitiveSetTyped("MAIN", [float], float)
 
     # rename arguments of the tree function
     pset.renameArguments(ARG0="k")
 
+    # add constants
+    pset.addTerminal(0.5, float, name="1/2")
+    pset.addTerminal(-1., float, name="-1.")
+    pset.addTerminal(1., float, name="1.")
+    pset.addTerminal(2., float, name="2.")
+
     penalty = config_file_data["gp"]['penalty']
 
     # define extra common arguments of fitness and predict functions
     common_params = {'penalty': penalty}
+
+    # set seed if needed
+    seed = None
 
     gpsr = gps.GPSymbolicRegressor(
         pset=pset, fitness=fitness.remote,
         error_metric=eval_MSE.remote, predict_func=predict.remote,
         feature_extractors=[len], print_log=True,
         common_data=common_params, config_file_data=config_file_data,
-        save_best_individual=True, save_train_fit_history=True,
+        save_best_individual=False, save_train_fit_history=False,
         plot_best_individual_tree=False,
-        output_path="./", batch_size=100)
+        output_path="./", batch_size=100, seed=seed)
 
     start = time.perf_counter()
-    gpsr.fit(train_data_i_j, val_data_i_j)
+    if config_file_data['gp']['validate']:
+        gpsr.fit(train_data_i_j, val_data_i_j)
+    else:
+        gpsr.fit(train_val_data_i_j)
 
     # recover the solution associated to the best individual among all the populations
     # comp_best = gpsr.predict(train_data)
 
-    # compute test error
-    # print(f"Test Error: {eval_MSE()}")
+    # compute and save test error
+    best_ind_str.append(str(gpsr.best))
+    ts_scores[i, j] = gpsr.score(test_data_i_j)
+
+    print("Best MSE on the test set: ", ts_scores[i, j])
 
     print(f"Elapsed time: {round(time.perf_counter() - start, 2)}")
+
+    print(gpsr.predict(train_val_data_i_j))
+    print(train_val_A_i_j)
+
+    print("------------------")
+
+    print(gpsr.predict(test_data_i_j))
+    print(test_A_i_j)
+
+    # np.savetxt("best_individuals.txt", np.array(best_ind_str), fmt="%s")
+    # np.savetxt("test_scores.txt", ts_scores)
 
 
 if __name__ == "__main__":
@@ -126,6 +163,7 @@ if __name__ == "__main__":
         config_file_data = yaml.safe_load(config_file)
 
     # load data
-    k_array, A_B_list = generate_toy_data(3)
+    # k_array, A_B_list = generate_toy_data(3)
+    k_array, A_B_list = process_data(5, "2dcyl")
     train_data, val_data, test_data = split_data(k_array, A_B_list)
     sr_rom(config_file_data, train_data, val_data, test_data)
