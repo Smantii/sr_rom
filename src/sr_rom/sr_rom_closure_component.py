cf00e852c0a8d63dfbd7c42b80a75ac0cdaa4789
Simone Manti <manti998@gmail.com>
2024-05-06 09:59:19 +0200

    Fixed sr_rom_closure_component.py

--- src/sr_rom/sr_rom_closure_component.py
+++ src/sr_rom/sr_rom_closure_component.py
@@ -24,30 +24,34 @@ num_cpus = 2
 
 
 def eval_MSE_sol(individual: Callable, indlen: int,
-                 k_component: Dataset) -> Tuple[float, List]:
+                 Re_data: Dataset) -> Tuple[float, List]:
 
     warnings.filterwarnings('ignore')
 
     config()
 
-    k_array = k_component.X
+    k_array = Re_data.X
+    A_0_0 = Re_data.y['A'][:, 0, 0]
+
     component_computed = individual(k_array)
-    total_error = jnp.mean((component_computed - k_component.y)**2)
+    total_error = jnp.mean((component_computed - A_0_0)**2)
 
     if jnp.isnan(total_error) or total_error > 1e6:
         total_error = 1e6
     return total_error, component_computed
 
 
-def eval_MSE_and_tune_constants(tree, toolbox, k_component: Dataset):
+def eval_MSE_and_tune_constants(tree, toolbox, Re_data: Dataset):
     warnings.filterwarnings("ignore")
     config()
     individual, n_constants = compile_individual_with_consts(tree, toolbox)
 
     def eval_err(consts):
-        k_array = k_component.X
+        k_array = Re_data.X
+        A_0_0 = Re_data.y['A'][:, 0, 0]
+
         component_computed = individual(k_array, consts)
-        total_error = jnp.mean((component_computed - k_component.y)**2)
+        total_error = jnp.mean((component_computed - A_0_0)**2)
         return total_error
 
     objective = jit(eval_err)
@@ -70,14 +74,13 @@ def eval_MSE_and_tune_constants(tree, toolbox, k_component: Dataset):
         # NLOPT solver
         prb = pg.problem(fitting_problem())
         algo = pg.algorithm(pg.nlopt(solver="lbfgs"))
-        algo.extract(pg.nlopt).ftol_abs = 1e-12
-        algo.extract(pg.nlopt).ftol_rel = 1e-12
-        algo.extract(pg.nlopt).maxeval = 5000
+        algo.extract(pg.nlopt).ftol_abs = 1e-4
+        algo.extract(pg.nlopt).ftol_rel = 1e-4
+        algo.extract(pg.nlopt).maxeval = 1000
         pop = pg.population(prb, size=0)
         pop.push_back(x0)
         pop = algo.evolve(pop)
         opt_result = algo.extract(pg.nlopt).get_last_opt_result()
-        print(opt_result)
         if (opt_result == 1) or (opt_result == 3) or (opt_result == 4):
             best_fit = pop.champion_f[0]
             best_consts = pop.champion_x
@@ -97,41 +100,41 @@ def eval_MSE_and_tune_constants(tree, toolbox, k_component: Dataset):
 
 @ray.remote(num_cpus=num_cpus)
 def eval_MSE(individuals_batch: list[gp.PrimitiveSet], indlen: int, toolbox: Toolbox,
-             k_component: Dataset, penalty: float) -> float:
+             Re_data: Dataset, penalty: float) -> float:
     objvals = [None]*len(individuals_batch)
 
     for i, individual in enumerate(individuals_batch):
         callable, _ = compile_individual_with_consts(individual, toolbox)
         def callable_with_consts(x): return callable(x, individual.consts)
-        objvals[i], _ = eval_MSE_sol(callable_with_consts, indlen, k_component)
+        objvals[i], _ = eval_MSE_sol(callable_with_consts, indlen, Re_data)
     return objvals
 
 
 @ray.remote(num_cpus=num_cpus)
 def predict(individuals_batch: list[gp.PrimitiveSet], indlen: int, toolbox: Toolbox,
-            k_component: Dataset, penalty: float) -> List:
+            Re_data: Dataset, penalty: float) -> List:
 
     best_sols = [None]*len(individuals_batch)
 
     for i, individual in enumerate(individuals_batch):
         callable, _ = compile_individual_with_consts(individual, toolbox)
         def callable_with_consts(x): return callable(x, individual.consts)
-        _, best_sols[i] = eval_MSE_sol(callable_with_consts, indlen, k_component)
+        _, best_sols[i] = eval_MSE_sol(callable_with_consts, indlen, Re_data)
 
     return best_sols
 
 
 @ray.remote(num_cpus=num_cpus)
 def fitness(individuals_batch: list[gp.PrimitiveSet], indlen: int, toolbox: Toolbox,
-            k_component: Dataset, penalty: float) -> Tuple[float, ]:
+            Re_data: Dataset, penalty: float) -> Tuple[float, ]:
 
     attributes = []*len(individuals_batch)
 
     for i, individual in enumerate(individuals_batch):
-        MSE, consts = eval_MSE_and_tune_constants(individual, toolbox, k_component)
+        MSE, consts = eval_MSE_and_tune_constants(individual, toolbox, Re_data)
 
         # callable = toolbox.compile(expr=individual)
-        # MSE, _ = eval_MSE_sol(callable, indlen, k_component)
+        # MSE, _ = eval_MSE_sol(callable, indlen, Re_data)
 
         # add penalty on length of the tree to promote simpler solutions
         fitness = (MSE + penalty["reg_param"]*len(individual),)
@@ -162,27 +165,10 @@ def assign_consts(individuals, attributes):
 
 
 def sr_rom(config_file_data, train_data, val_data, test_data, output_path):
-    best_ind_str = []
-    ts_scores = jnp.zeros((5, 5), dtype=jnp.float64)
-
     # for i in range(5):
     #    for j in range(5):
-    i = 0
-    j = 0
-
-    train_A_i_j = [A_B['A'][i, j]for A_B in train_data.y]
-    val_A_i_j = [A_B['A'][i, j]for A_B in val_data.y]
-    test_A_i_j = [A_B['A'][i, j]for A_B in test_data.y]
-    train_val_A_i_j = train_A_i_j + val_A_i_j
-    train_data_i_j = Dataset("k_component", jnp.array(
-        train_data.X), jnp.array(train_A_i_j))
-    val_data_i_j = Dataset("k_component", jnp.array(val_data.X), jnp.array(val_A_i_j))
-    test_data_i_j = Dataset("k_component", jnp.array(
-        test_data.X), jnp.array(test_A_i_j))
-    train_val_data_i_j = Dataset(
-        "k_component", jnp.array(train_data.X + val_data.X), jnp.array(train_val_A_i_j))
-
-    # print(train_data_i_j.X, val_data_i_j.X, test_data_i_j.X)
+    # i = 0
+    # j = 0
 
     pset = gp.PrimitiveSetTyped("MAIN", [float], float)
 
@@ -207,29 +193,26 @@ def sr_rom(config_file_data, train_data, val_data, test_data, output_path):
         common_data=common_params, config_file_data=config_file_data,
         save_best_individual=True, save_train_fit_history=True,
         plot_best_individual_tree=False, callback_func=assign_consts,
-        output_path=output_path, batch_size=200, seed=seed)
+        output_path=output_path, batch_size=1, seed=seed)
 
     start = time.perf_counter()
     if config_file_data['gp']['validate']:
-        gpsr.fit(train_data_i_j, val_data_i_j)
+        gpsr.fit(train_data, val_data)
     else:
-        gpsr.fit(train_val_data_i_j)
+        gpsr.fit(train_val_data)
 
     # recover the solution associated to the best individual among all the populations
     # comp_best = gpsr.predict(train_data)
 
-    # compute and save test error
-    best_ind_str.append(str(gpsr.best))
-    ts_scores = ts_scores.at[i, j].set(gpsr.score(test_data_i_j))
+    # compute test error
+    print("Best MSE on the test set: ", gpsr.score(test_data))
 
-    print("Best MSE on the test set: ", ts_scores[i, j])
-
-    print(f"Elapsed time: {round(time.perf_counter() - start, 2)}")
+    print("Best constants = ", gpsr.best.consts)
 
     # train_computed = gpsr.predict(train_data_i_j)
     # val_computed = gpsr.predict(val_data_i_j)
-    train_val_computed = gpsr.predict(train_val_data_i_j)
-    test_computed = gpsr.predict(test_data_i_j)
+    train_val_computed = gpsr.predict(train_val_data)
+    test_computed = gpsr.predict(test_data)
 
     # print(train_computed)
     # print(train_A_i_j)
@@ -238,38 +221,28 @@ def sr_rom(config_file_data, train_data, val_data, test_data, output_path):
     # print(val_A_i_j)
 
     print(train_val_computed)
-    print(train_val_A_i_j)
+    print(train_val_data.y['A'][:, 0, 0])
 
     print("------------------")
 
     print(test_computed)
-    print(test_A_i_j)
+    print(test_data.y['A'][:, 0, 0])
 
     print("Best constants = ", gpsr.best.consts)
 
-    k_data = np.concatenate((train_val_data_i_j.X, test_data_i_j.X))
+    k_data = np.concatenate((train_val_data.X, test_data.X))
 
     A_i_j_computed = np.concatenate((train_val_computed, test_computed))
 
     # NOTE:only for plot
-    # FIXME: do a separate func
-    ordered_idx = np.argsort(k_data)
-    ordered_train_val_idx = np.argsort(train_val_data_i_j.X)
-    ordered_test_idx = np.argsort(test_data_i_j.X)
-    k_train_val_ord = train_val_data_i_j.X[ordered_train_val_idx]
-    k_test_ord = test_data_i_j.X[ordered_test_idx]
-    A_i_j_train_val_ord = np.array(train_val_A_i_j)[ordered_train_val_idx]
-    A_i_j_test_ord = np.array(test_A_i_j)[ordered_test_idx]
-    k_ord = k_data[ordered_idx]
-    # A_i_j_computed_ord = A_i_j_computed[ordered_idx]
-
-    k_sample = np.linspace(min(k_ord), max(k_ord), 1001)
-    data = Dataset("k_component", k_sample, np.zeros_like(k_sample))
+    k_sample = np.linspace(min(k_data), max(k_data), 1001)
+    data = Dataset("Re_data", k_sample, {'A': np.zeros((1001, 5, 5))})
     A_i_j_computed = gpsr.predict(data)
 
-    plt.scatter(k_train_val_ord, A_i_j_train_val_ord,
+    plt.scatter(train_val_data.X, train_val_data.y['A'][:, 0, 0],
                 c="#b2df8a", marker=".", label="Training data")
-    plt.scatter(k_test_ord, A_i_j_test_ord, c="#b2df8a", marker="*", label="Test data")
+    plt.scatter(test_data.X, test_data.y['A'][:, 0, 0],
+                c="#b2df8a", marker="*", label="Test data")
     plt.plot(k_sample, A_i_j_computed, c="#1f78b4", label="Best solution")
     # plt.plot(k_ord, A_i_j_computed_ord, c="#1f78b4", label="Best solution")
     plt.xlabel(r"$Re$")
@@ -281,8 +254,7 @@ def sr_rom(config_file_data, train_data, val_data, test_data, output_path):
 
     plt.savefig("data_vs_sol.pdf", dpi=300)
 
-    np.savetxt("best_individuals.txt", np.array(best_ind_str), fmt="%s")
-    np.savetxt("test_scores.txt", ts_scores)
+    print(f"Elapsed time: {round(time.perf_counter() - start, 2)}")
 
 
 if __name__ == "__main__":
@@ -294,8 +266,9 @@ if __name__ == "__main__":
     # load data
     # from sr_rom.data.data import generate_toy_data
     # k_array, A_B_list = generate_toy_data(5)
-    k_array, A_B_list = process_data(5, "2dcyl/Re200_300_rank5")
-    train_data, val_data, test_data = split_data(k_array, A_B_list)
+    Re, A, B, tau, a_FOM = process_data(5, "2dcyl/Re200_300")
+    train_data, val_data, train_val_data, test_data = split_data(
+        Re, 1000*A, 1000*B, tau, a_FOM)
 
     if n_args >= 3:
         output_path = sys.argv[2]
