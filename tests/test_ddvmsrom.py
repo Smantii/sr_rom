2c8fa69560ada44d22e152fc7242e534a422efbc
Ping-Hsuan <pht2@illinois.edu>
2024-07-05 15:59:46 -0500

    remove redundant lines

--- tests/test_ddvmsrom.py
+++ tests/test_ddvmsrom.py
@@ -2,7 +2,6 @@ import pytest
 import numpy as np
 import pandas as pd
 from src.sr_rom.code.utils import (
-    find_snapshot_rank, 
     load_rom_ops, 
     get_rom_ops_r_dim, 
     load_coefficients_training_data, 
@@ -19,26 +18,18 @@ def test_ddvmsrom():
     r = 5
 
     sdir = "./tests/data/ops"
-
-    R = find_snapshot_rank(sdir)
-    print(f'Snapshot rank is: {R}')
     
     a0_full, b0_full, cu_full, mb = load_rom_ops(sdir)
-    print("done loading ... ")
     
     # ROM operators in R dimensional space
     au0_, bu0_, au_, bu_, cu_, cutmp_, cutmp1_ = get_rom_ops_r_dim(a0_full, b0_full, cu_full, mb)
     
-    rows = []
     # ROM operators in r dimensional space
     au0, bu0, au, bu, cu, cutmp, cutmp1 = get_rom_ops_r_dim(a0_full, b0_full, cu_full, r)
     u0 = load_initial_condition(sdir, r)
     
     # Coefficients of the projected snapshots onto mb dimensional space 
     uk, ns = load_coefficients_training_data(sdir, mb)
-    
-    u = np.zeros((r+1, 3))
-    u[:, 0] = u0
     
     path_to_model = "src/sr_rom/results_extrapolation/sr/results_20/results_w_3_n_2/"
     istep, ucoef = rom_online_solver_wclosure(a0_full, b0_full, au0, bu0, au, bu, cu, cutmp, u0, uk, nsteps, iostep, mu, dt, r, "SR", path_to_model)
