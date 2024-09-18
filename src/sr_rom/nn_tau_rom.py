Fixed Re normalization issue.

src/sr_rom/nn_tau_rom.py
@@ -33,17 +33,6 @@ class NeuralNetwork(nn.Module):
         return logits
 
 
-class RegularizedNet(NeuralNetwork):
-    def __init__(self, *args, lambda1=0.01, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.lambda1 = lambda1
-
-    def get_loss(self, y_pred, y_true, X=None, training=False):
-        loss = super().get_loss(y_pred, y_true, X=X, training=training)
-        loss += self.lambda1 * sum([w.abs().sum() for w in self.module_.parameters()])
-        return loss
-
-
 output_path = sys.argv[1]
 seed = 42
 np.random.seed(seed)
@@ -74,22 +63,22 @@ t = np.linspace(500, 520, 2001)
 
 for i in range(r):
     idx_train = np.argsort(train_val_data.y["X_sampled"][:, 0])
+    idx_test = np.argsort(test_data.y["X"][:, 0])
     y_train = train_val_data.y["tau"][:, ::t_sample, i].flatten("F")[idx_train]
-    y_test = test_data.y["tau"][:, :, i].flatten("F")
+    y_test = test_data.y["tau"][:, :, i].flatten("F")[idx_test]
 
     # Standardization
-    max_min_X_train = [np.max(train_val_data.y["X_sampled"][idx_train, :], axis=0),
-                       np.min(train_val_data.y["X_sampled"][idx_train, :], axis=0)]
-    max_min_train_comp = [np.max(y_train, axis=0),
-                          np.min(y_train, axis=0)]
-    X_train_norm = (train_val_data.y["X_sampled"][idx_train, :] -
-                    max_min_X_train[1])/(max_min_X_train[0] - max_min_X_train[1])
-    y_train_norm = (y_train - max_min_train_comp[1]) / \
-        (max_min_train_comp[0] - max_min_train_comp[1])
-    X_test_norm = (test_data.y["X"][:, :] - max_min_X_train[1]
-                   )/(max_min_X_train[0] - max_min_X_train[1])
-    y_test_norm = (y_test - max_min_train_comp[1]) / \
-        (max_min_train_comp[0] - max_min_train_comp[1])
+    mean_std_X_train = [np.mean(train_val_data.y["X_sampled"][idx_train, 1:], axis=0),
+                        np.std(train_val_data.y["X_sampled"][idx_train, 1:], axis=0)]
+    X_train_norm = train_val_data.y["X_sampled"][idx_train]
+    X_test_norm = test_data.y["X"][idx_test]
+    X_train_norm[:, 0] /= 1000
+    X_test_norm[:, 0] /= 1000
+    X_train_norm[:, 1:] = (X_train_norm[:, 1:] -
+                           mean_std_X_train[0])/mean_std_X_train[1]
+    X_test_norm[:, 1:] = (X_test_norm[:, 1:] - mean_std_X_train[0])/mean_std_X_train[1]
+    y_train_norm = y_train
+    y_test_norm = y_test
 
     X_train_norm = torch.from_numpy(X_train_norm).to(torch.float32)
     y_train_norm = torch.from_numpy(y_train_norm.reshape(-1, 1)).to(torch.float32)
@@ -97,12 +86,11 @@ for i in range(r):
     y_test_norm = torch.from_numpy(y_test_norm.reshape(-1, 1)).to(torch.float32)
 
     model = NeuralNetRegressor(module=NeuralNetwork, batch_size=512, verbose=0,
-                               optimizer=torch.optim.Adam, max_epochs=200,
+                               optimizer=torch.optim.Adam, max_epochs=1000,
                                train_split=None, device="cuda", iterator_train__shuffle=True)
 
     params = {'lr': [1e-4, 1e-3],
               'optimizer__weight_decay': [1e-5, 1e-4, 1e-3],
-              # 'module__lambda1': [1e-6, 1e-5, 1e-4, 1e-3],
               'module__hidden_units': [[64, 128, 256, 128, 64],
                                        [64, 128, 256, 512, 256, 128, 64],
                                        [128, 256, 512, 1024, 512, 256, 128]],
@@ -125,15 +113,15 @@ for i in range(r):
     print(f"The R^2 in the training set is {r2_train.item()}", flush=True)
     print(f"The R^2 in the test set is {r2_test.item()}", flush=True)
 
-    '''
     # plot prediction
-    X_scaled = (X[:, :] - mean_std_X_train[0])/mean_std_X_train[1]
+    X_scaled = X
+    X_scaled[:, 0] /= 1000
+    X_scaled[:, 1:] = (X[:, 1:] - mean_std_X_train[0])/mean_std_X_train[1]
 
     model_out = gs.predict(torch.from_numpy(X_scaled).to(torch.float32).cuda())
 
     # rescale out back
-    model_out_np = model_out*mean_std_train_comp[1] + mean_std_train_comp[0]
-    pred = model_out_np.reshape((num_Re, num_t), order="F")
+    pred = model_out.reshape((num_Re, num_t), order="F")
 
     re_mesh, t_mesh = np.meshgrid(Re, t)
 
@@ -158,4 +146,3 @@ for i in range(r):
     model_out_reshaped = model_out.reshape((num_Re, num_t), order="F")
     np.save(output_path + "model_pred_" + str(i) + ".npy", model_out_reshaped)
     gs.best_estimator_.save_params(output_path + "model_param_" + str(i) + ".pkl")
-    '''

src/sr_rom/sr_rom_operon_tau.py
@@ -14,7 +14,7 @@ warnings.filterwarnings("ignore")
 
 
 def save_results(reg, X, tau, X_train_val, y_train_val, X_test, y_test,
-                 mean_std_train_Re, mean_std_train_comp, prb_name, ylabel, output_path, learning_time):
+                 mean_std_X_train, prb_name, ylabel, output_path, learning_time):
 
     train_val_score = reg.score(X_train_val, y_train_val)
     test_score = reg.score(X_test, y_test)
@@ -23,16 +23,18 @@ def save_results(reg, X, tau, X_train_val, y_train_val, X_test, y_test,
 
     # reconstruct the full dataset
     num_t = 2001
-    num_re_train_val = int(len(X_train_val[:, 0])/num_t)
+    num_t_sample = 11
+    num_re_train_val = int(len(X_train_val[:, 0])/num_t_sample)
     num_re_test = int(len(X_test[:, 0])/num_t)
     num_re = num_re_train_val + num_re_test
 
     # model prediction
-    X_scaled = (X[:, 1:] - mean_std_train_Re[0])/mean_std_train_Re[1]
+    X_scaled = X
+    X_scaled[:, 0] /= 1000
+    X_scaled[:, 1:] = (X[:, 1:] - mean_std_X_train[0])/mean_std_X_train[1]
     model_out = reg.predict(X_scaled)
 
     # revert data scaling
-    model_out = model_out*mean_std_train_comp[1] + mean_std_train_comp[0]
     prediction = model_out.reshape((num_re, num_t), order="F")
 
     with open(output_path + "models.txt", "a") as text_file:
@@ -84,32 +86,30 @@ def sr_rom_operon(train_val_data, test_data, X, tau, t_sample, output_path):
         }
 
     print("Started training procedure for tau", flush=True)
-    idx_train_sampled = np.argsort(train_val_data.y["X_sampled"][:, 0])
+    idx_train = np.argsort(train_val_data.y["X_sampled"][:, 0])
+    idx_test = np.argsort(test_data.y["X"][:, 0])
+
     num_trials = 10
     r = X.shape[1] - 1
     # training procedure for tau
     for i in range(r):
-        y_train = train_val_data.y["tau"][:, :, i].flatten("F")
-        y_train_sampled = train_val_data.y["tau"][:, ::t_sample, i].flatten("F")[
-            idx_train_sampled]
-        y_test = test_data.y["tau"][:, :, i].flatten("F")
+        y_train = train_val_data.y["tau"][:, ::t_sample, i].flatten("F")[
+            idx_train]
+        y_test = test_data.y["tau"][:, :, i].flatten("F")[idx_test]
 
         # Standardization
-        mean_std_X_train = [np.mean(train_val_data.y["X_sampled"][:, 1:], axis=0),
-                            np.std(train_val_data.y["X_sampled"][:, 1:], axis=0)]
-        mean_std_train_comp = [np.mean(y_train, axis=0),
-                               np.std(y_train, axis=0)]
-        X_train_norm = (train_val_data.y["X"][:, 1:] -
-                        mean_std_X_train[0])/mean_std_X_train[1]
-        X_sampled_train_norm = (
-            train_val_data.y["X_sampled"][idx_train_sampled, 1:] - mean_std_X_train[0])/mean_std_X_train[1]
-        y_train_norm = (y_train - mean_std_train_comp[0]) / \
-            mean_std_train_comp[1]
-        y_sampled_train_norm = (y_train_sampled - mean_std_train_comp[0]) / \
-            mean_std_train_comp[1]
-        X_test_norm = (test_data.y["X"][:, 1:] -
-                       mean_std_X_train[0])/mean_std_X_train[1]
-        y_test_norm = (y_test - mean_std_train_comp[0])/mean_std_train_comp[1]
+        mean_std_X_train = [np.mean(train_val_data.y["X_sampled"][idx_train, 1:], axis=0),
+                            np.std(train_val_data.y["X_sampled"][idx_train, 1:], axis=0)]
+        X_train_norm = train_val_data.y["X_sampled"][idx_train]
+        X_test_norm = test_data.y["X"][idx_test]
+        X_train_norm[:, 0] /= 1000
+        X_test_norm[:, 0] /= 1000
+        X_train_norm[:, 1:] = (X_train_norm[:, 1:] -
+                               mean_std_X_train[0])/mean_std_X_train[1]
+        X_test_norm[:, 1:] = (X_test_norm[:, 1:] -
+                              mean_std_X_train[0])/mean_std_X_train[1]
+        y_train_norm = y_train
+        y_test_norm = y_test
 
         gs = GridSearchCV(SymbolicRegressor(), params, cv=5, verbose=0, refit=False,
                           n_jobs=-1, return_train_score=False)
@@ -117,7 +117,7 @@ def sr_rom_operon(train_val_data, test_data, X, tau, t_sample, output_path):
         tic = time.time()
         for trial in range(num_trials):
             print(f"GS number {trial}", flush=True)
-            gs.fit(X_sampled_train_norm, y_sampled_train_norm)
+            gs.fit(X_train_norm, y_train_norm)
             if trial == 0:
                 params_results["params"] = gs.cv_results_["params"]
                 # init mean test scores
@@ -134,12 +134,11 @@ def sr_rom_operon(train_val_data, test_data, X, tau, t_sample, output_path):
         best_params = params_results["params"][best_params_idx]
         print(best_params, flush=True)
         reg = SymbolicRegressor(**best_params)
-        reg.fit(X_sampled_train_norm, y_sampled_train_norm)
+        reg.fit(X_train_norm, y_train_norm)
         toc = time.time()
 
         save_results(reg, X, tau[:, :, i], X_train_norm, y_train_norm, X_test_norm, y_test_norm,
-                     mean_std_X_train, mean_std_train_comp,
-                     "tau_" + str(i), r"$\tau_i$", output_path, toc-tic)
+                     mean_std_X_train, "tau_" + str(i), r"$\tau_i$", output_path, toc-tic)
 
 
 if __name__ == "__main__":
@@ -149,7 +148,7 @@ if __name__ == "__main__":
     t_sample = 200
     r = 2
     Re, A, B, tau, a_FOM, X, X_sampled, residual = process_data(
-        r, "2dcyl/Re200_300", t_sample)
+        r, "2dcyl/Re200_400", t_sample)
 
     for w in windows:
         print(f"---Collecting results for window size {w}...!---", flush=True)